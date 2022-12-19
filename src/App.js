32b30d3d843c963c0f9011bf9b077bfe1547a213
Isabella <isacosta90a@gmail.com>
2022-12-19 14:38:35 -0300

    pág 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Logo from './assets/images/logo.svg'
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main className='rodape-superior'>
+        <img src={Logo} />
+        <p>Investir</p>
+        <p>Quanto custa</p>
+        <p>Sobre</p>
+        <p>Aprender</p>
+        <p>Magazine</p>
+        <p>Ferramentas</p>
+        <p>Login</p>
+        <button className='abrir-conta'>Abra sua conta</button>
+      </main>
+
+      <section className='faixa-principal'>
+      <div className='pequenos-textos'>
+          <h1 className='text-1'>Invista em viver.</h1>
+          <p className='pp'>Investir toma tempo, mas não precisa ser o seu.</p>
+          <p className='pp'>Conte com nossos especialistas.</p>
+          <button className='comece-agora'>Comece agora</button>
+        </div>
+      </section>
     </div>
   );
 }
